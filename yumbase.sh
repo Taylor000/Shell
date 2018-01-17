842e725970a1588600892c05756cf2fe442231a1
阿拉凹凸曼 <zgw505@gmail.com>
2018-01-17 15:21:01 +0800

    添加命令
    1、添加重启任务服务
    2、将时间写入BIOS

--- yumbase.sh
+++ yumbase.sh
@@ -2,9 +2,13 @@
 yum update -y
 #安装常用基础软件
 yum install vim lrzsz screen git ntp crontabs -y
-#设置时区
+#设置时区为东八区
 echo yes | cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 #同步时间
 ntpdate cn.pool.ntp.org
-#添加系统定时任务自动同步时间
+#添加系统定时任务自动同步时间并重启定时任务服务
 sed -i '$a\* * * * 1 ntpdate cn.pool.ntp.org >> /dev/null 2>&1' /etc/crontab
+service crond restart
+#/etc/init.d/crond restart
+#把时间写入到BIOS
+hwclock -w
