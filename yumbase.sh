69691c5625ae5c46e379d71c67af8e9b40a60948
阿拉凹凸曼 <zgw505@gmail.com>
2018-01-20 11:33:41 +0800

    添加安装net-tools

--- yumbase.sh
+++ yumbase.sh
@@ -1,7 +1,7 @@
 #更新系统
 yum update -y
 #安装常用基础软件
-yum install vim lrzsz screen git ntp crontabs -y
+yum install vim lrzsz screen git ntp crontabs net-tools -y
 #设置时区为东八区
 echo yes | cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 #同步时间
