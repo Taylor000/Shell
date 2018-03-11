87a69db9ffeace49f730b7d5baf4eb784e7bd075
阿拉凹凸曼 <zgw505@gmail.com>
2018-03-11 18:30:01 +0800

    自用一键安装脚本

--- deploy_ssr_node.sh
+++ deploy_ssr_node.sh
@@ -1,43 +1,70 @@
-#!/usr/bin/env bash
-# Require Root Permission
-# SSR node deploy script
-# Author: Hao-Luo (https://github.com/Hao-Luo/Others/tree/master/apps/ssr)
-# Editor: SSRPanel
-
+#!/bin/bash
 [ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
-
 install_ssr(){
+	clear
+	stty erase '^H' && read -p " mysql服务器密码:" sspass
+	stty erase '^H' && read -p " SSR节点ID（nodeid）:" ssnode
+	
+	clear
 	cd /root/
-	yum -y install git
-	echo 'SSR下载中...'
-	wget -c https://github.com/ssrpanel/ssrpanel/raw/master/server/ssr-3.4.0.tar.gz && tar -zxvf ssr-3.4.0.tar.gz && cd shadowsocksr &&  ./setup_cymysql.sh
-	echo '开始配置节点连接信息...'
-	stty erase '^H' && read -p "数据库服务器地址:" mysqlserver
-	stty erase '^H' && read -p "数据库服务器端口:" port
-	stty erase '^H' && read -p "数据库名称:" database
-	stty erase '^H' && read -p "数据库用户名:" username
-	stty erase '^H' && read -p "数据库密码:" pwd
-	stty erase '^H' && read -p "本节点ID:" nodeid
-	stty erase '^H' && read -p "本节点流量计算比例:" ratio
-	sed -i -e "s/server_host/$mysqlserver/g" usermysql.json
-	sed -i -e "s/server_port/$port/g" usermysql.json
-	sed -i -e "s/server_db/$database/g" usermysql.json
-	sed -i -e "s/server_user/$username/g" usermysql.json
-	sed -i -e "s/server_password/$pwd/g" usermysql.json
-	sed -i -e "s/nodeid/$nodeid/g" usermysql.json
-	sed -i -e "s/noderatio/$ratio/g" usermysql.json
-	echo -e "配置完成!\n如果无法连上数据库，请检查本机防火墙或者数据库防火墙!\n请自行编辑user-config.json，配置节点加密方式、混淆、协议等"
+  	wget https://github.com/jedisct1/libsodium/releases/download/1.0.16/libsodium-1.0.16.tar.gz
+  	tar xf libsodium-1.0.16.tar.gz && cd libsodium-1.0.16
+  	./configure && make -j2 && make install
+  	echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
+	cd /root/
+  	rm -rf libsodium-1.0.16.tar.gz
+	echo 'libsodium安装完成'
+	
+  	git clone -b master https://github.com/828768/shadowsocksr.git && cd shadowsocksr && sh setup_cymysql.sh && sh initcfg.sh
+	echo 'ssr安装完成'
+
+	sed -i -e "s/ssserver/db\.wubase\.cn/g" usermysql.json
+	sed -i -e "s/ssport/3306/g" usermysql.json
+	sed -i -e "s/ssuser/ssrpanel/g" usermysql.json
+	sed -i -e "s/sspass/$sspass/g" usermysql.json
+	sed -i -e "s/ssdb/ssrpanel/g" usermysql.json
+	sed -i -e "s/ssnode/$ssnode/g" usermysql.json
+	echo 'ssr配置完成'
+	chmod +x run.sh && ./run.sh
+	cd /root/
+	echo 'ssr已开始运行'
+	
+	service iptables stop
+	service firewalld stop
+	systemctl disable firewalld.service
+	chkconfig iptables off
+	echo '已关闭iptables、firewalld，如有需要请自行配置。'
 }
 
 open_bbr(){
+	clear
 	cd
 	wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
 	chmod +x bbr.sh
 	./bbr.sh
 }
 
-echo -e "1.Install SSR\n2.Open BBR"
-stty erase '^H' && read -p "请输入数字进行安装[1-2]:" num
+auto_reboot(){
+	clear
+	echo '设置每天几点几分重启节点'
+	stty erase '^H' && read -p " 小时(0-23):" hour
+	stty erase '^H' && read -p " 分钟(0-59):" minute
+	chmod +x /etc/rc.d/rc.local
+	echo /sbin/service crond start >> /etc/rc.d/rc.local
+	echo "/root/shadowsocksr/run.sh" >> /etc/rc.d/rc.local
+	echo '设置开机运行SSR'
+	echo "$minute $hour * * * root /sbin/reboot" >> /etc/crontab
+	service crond start
+}
+
+yum -y install git
+yum -y groupinstall "Development Tools"
+clear
+echo ' 注意：此脚本基于centos7编写，其他系统可能会出问题'
+echo ' 1. 安装 SSR'
+echo ' 2. 安装 BBR'
+echo ' 3. 设置定时重启（测试中）'
+stty erase '^H' && read -p " 请输入数字 [1-3]:" num
 case "$num" in
 	1)
 	install_ssr
@@ -45,7 +72,10 @@ case "$num" in
 	2)
 	open_bbr
 	;;
+	3)
+	auto_reboot
+	;;
 	*)
-	echo "请输入正确数字[1-2]:"
+	echo '请输入正确数字 [1-3]'
 	;;
 esac
