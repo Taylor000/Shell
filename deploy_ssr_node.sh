c2f981e7cb3d7ff4e62e48ad891e7b4778feec71
阿拉凹凸曼 <zgw505@gmail.com>
2018-04-01 13:46:14 +0800

    更改路径

--- deploy_ssr_node.sh
+++ deploy_ssr_node.sh
@@ -6,12 +6,12 @@ install_ssr(){
 	stty erase '^H' && read -p " SSR节点ID（nodeid）:" ssnode
 	
 	clear
-	cd /root/
+	cd /usr/
   	wget https://github.com/jedisct1/libsodium/releases/download/1.0.16/libsodium-1.0.16.tar.gz
   	tar xf libsodium-1.0.16.tar.gz && cd libsodium-1.0.16
   	./configure && make -j2 && make install
   	echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
-	cd /root/
+	cd /usr/
   	rm -rf libsodium-1.0.16.tar.gz
 	echo 'libsodium安装完成'
 	
@@ -27,8 +27,8 @@ install_ssr(){
 	echo 'ssr配置完成'
 	chmod +x run.sh && ./run.sh
 	#开机自动运行
-	echo "/root/shadowsocksr/run.sh" >> /etc/rc.d/rc.local
-	cd /root/
+	echo "/usr/shadowsocksr/run.sh" >> /etc/rc.d/rc.local
+	cd /usr/
 	echo 'ssr已开始运行'
 	
 	service iptables stop
@@ -41,7 +41,7 @@ install_ssr(){
 open_bbr(){
 	clear
 	cd
-	wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
+	wget --no-check-certificate -O bbr.sh https://github.com/teddysun/across/raw/master/bbr.sh
 	chmod +x bbr.sh
 	./bbr.sh
 }
@@ -53,7 +53,7 @@ auto_reboot(){
 	stty erase '^H' && read -p " 分钟(0-59):" minute
 	chmod +x /etc/rc.d/rc.local
 	echo /sbin/service crond start >> /etc/rc.d/rc.local
-	echo "/root/shadowsocksr/run.sh" >> /etc/rc.d/rc.local
+	echo "/usr/shadowsocksr/run.sh" >> /etc/rc.d/rc.local
 	echo '设置开机运行SSR'
 	echo "$minute $hour * * * root /sbin/reboot" >> /etc/crontab
 	service crond start
@@ -65,7 +65,7 @@ clear
 echo ' 注意：此脚本基于centos7编写，其他系统可能会出问题'
 echo ' 1. 安装 SSR'
 echo ' 2. 安装 BBR'
-echo ' 3. 设置定时重启（测试中）'
+echo ' 3. 设置定时重启'
 stty erase '^H' && read -p " 请输入数字 [1-3]:" num
 case "$num" in
 	1)
