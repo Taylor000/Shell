b3a713221a12fada757d8269401f819b64ab15e1
阿拉凹凸曼 <zgw505@gmail.com>
2018-04-02 16:47:47 +0800

    Update yumbase.sh

--- yumbase.sh
+++ yumbase.sh
@@ -7,7 +7,7 @@ echo yes | cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 #同步时间
 ntpdate cn.pool.ntp.org
 #添加系统定时任务自动同步时间并重启定时任务服务
-sed -i '/*ntpdate*/d' /etc/crontab
+sed -i '/^.*ntpdate*/d' /etc/crontab
 sed -i '$a\* * * * 1 ntpdate cn.pool.ntp.org >> /dev/null 2>&1' /etc/crontab
 service crond restart
 #/etc/init.d/crond restart
